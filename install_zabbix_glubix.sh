f9efee2cc07d4397109de1726de01cc51d07628b
smsgaucho <julioagostini@gmail.com>
2016-09-22 16:32:09 -0300

    Update install_zabbix_glubix.sh

--- install_zabbix_glubix.sh
+++ install_zabbix_glubix.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+## Instalar os 3 comandos abaixo para instalar o zabbix_glubix para monitorar o glusterfs
+#wget https://github.com/julioagostini/julioagostini/blob/master/install_zabbix_glubix.sh
+#chmod +x install_zabbix_glubix.sh
+#./install_zabbix_glubix.sh
+
 files="glubix_checkvolstatus.pl glubix_georepstatus33.pl glubix_georepstatus34.pl glubix_numpeers.pl glubix_uuid.pl glubix_volstatus.pl glubix_voltype.pl"
 for file in $files
 do
