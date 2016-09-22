37b8b8ed66039b4c1da08ba93788cda04253d248
smsgaucho <julioagostini@gmail.com>
2016-09-22 16:35:00 -0300

    Update install_zabbix_glubix.sh

--- install_zabbix_glubix.sh
+++ install_zabbix_glubix.sh
@@ -7,7 +7,7 @@
 files="glubix_checkvolstatus.pl glubix_georepstatus33.pl glubix_georepstatus34.pl glubix_numpeers.pl glubix_uuid.pl glubix_volstatus.pl glubix_voltype.pl"
 for file in $files
 do
-   #wget https://github.com/htaira/glubix/blob/master/userparams/${file} -O /usr/libexec/${file}
+   wget https://github.com/htaira/glubix/blob/master/userparams/${file} -O /usr/libexec/${file}
    chmod +x /usr/libexec/${file}
 done
 service zabbix-agent restart
