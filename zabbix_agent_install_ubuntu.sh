545558cca8c58d82b189b3de72bab86f14ad3b26
smsgaucho <julioagostini@gmail.com>
2022-06-13 17:47:45 -0300

    Update zabbix_agent_install_ubuntu.sh

--- zabbix_agent_install_ubuntu.sh
+++ zabbix_agent_install_ubuntu.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-HOSTNAME="$1"
+SERVER="$1"
+HOSTNAME="$2"
 OSVERSION=$(cat /etc/*release | grep VERSION_CODENAME | awk -F= '{print $2}')
 wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+${OSVERSION}_all.deb
 dpkg -i zabbix-release_5.0-1+${OSVERSION}_all.deb
 apt update
 apt install zabbix-agent
 
-echo "PidFile=/var/run/zabbix/zabbix_agentd.pid
-LogFile=/var/log/zabbix/zabbix_agentd.log
-LogFileSize=0
-Server=172.31.3.144
-ServerActive=172.31.3.144
-Hostname=$HOSTNAME
-Include=/etc/zabbix/zabbix_agentd.d/*.conf" > /etc/zabbix/zabbix_agentd.conf
+echo "PidFile = /var/run/zabbix/zabbix_agentd.pid
+LogFile = /var/log/zabbix/zabbix_agentd.log
+LogFileSize = 0
+Server = $SERVER
+ServerActive = $SERVER
+Hostname = $HOSTNAME
+Include = /etc/zabbix/zabbix_agentd.d/*.conf" > /etc/zabbix/zabbix_agentd.conf
 
 
 systemctl stop zabbix-agent.service
